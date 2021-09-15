136e2add26cb2dc12087ad7b74b9140f4272b238
tmampa <tmampa94@gmail.com>
2021-09-15 14:02:45 +0200

    Fix linters

--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -1,82 +1,77 @@
-import { compareAsc, toDate } from 'date-fns'
-import Project from './Project'
-import Task from './Task'
+import { compareAsc, toDate } from 'date-fns';
+import Project from './Project.js';
+import Task from './Task.js';
 
 export default class TodoList {
   constructor() {
-    this.projects = []
-    this.projects.push(new Project('Inbox'))
-    this.projects.push(new Project('Today'))
-    this.projects.push(new Project('This week'))
+    this.projects = [];
+    this.projects.push(new Project('Inbox'));
+    this.projects.push(new Project('Today'));
+    this.projects.push(new Project('This week'));
   }
 
   setProjects(projects) {
-    this.projects = projects
+    this.projects = projects;
   }
 
   getProjects() {
-    return this.projects
+    return this.projects;
   }
 
   getProject(projectName) {
-    return this.projects.find((project) => project.getName() === projectName)
+    return this.projects.find((project) => project.getName() === projectName);
   }
 
   contains(projectName) {
-    return this.projects.some((project) => project.getName() === projectName)
+    return this.projects.some((project) => project.getName() === projectName);
   }
 
   addProject(newProject) {
-    if (this.projects.find((project) => project.name === newProject.name))
-      return
-    this.projects.push(newProject)
+    if (this.projects.find((project) => project.name === newProject.name)) return;
+    this.projects.push(newProject);
   }
 
   deleteProject(projectName) {
     const projectToDelete = this.projects.find(
-      (project) => project.getName() === projectName
-    )
-    this.projects.splice(this.projects.indexOf(projectToDelete), 1)
+      (project) => project.getName() === projectName,
+    );
+    this.projects.splice(this.projects.indexOf(projectToDelete), 1);
   }
 
   updateTodayProject() {
-    this.getProject('Today').tasks = []
+    this.getProject('Today').tasks = [];
 
     this.projects.forEach((project) => {
-      if (project.getName() === 'Today' || project.getName() === 'This week')
-        return
+      if (project.getName() === 'Today' || project.getName() === 'This week') return;
 
-      const todayTasks = project.getTasksToday()
+      const todayTasks = project.getTasksToday();
       todayTasks.forEach((task) => {
-        const taskName = `${task.getName()} (${project.getName()})`
-        this.getProject('Today').addTask(new Task(taskName, task.getDate()))
-      })
-    })
+        const taskName = `${task.getName()} (${project.getName()})`;
+        this.getProject('Today').addTask(new Task(taskName, task.getDate()));
+      });
+    });
   }
 
   updateWeekProject() {
-    this.getProject('This week').tasks = []
+    this.getProject('This week').tasks = [];
 
     this.projects.forEach((project) => {
-      if (project.getName() === 'Today' || project.getName() === 'This week')
-        return
+      if (project.getName() === 'Today' || project.getName() === 'This week') return;
 
-      const weekTasks = project.getTasksThisWeek()
+      const weekTasks = project.getTasksThisWeek();
       weekTasks.forEach((task) => {
-        const taskName = `${task.getName()} (${project.getName()})`
-        this.getProject('This week').addTask(new Task(taskName, task.getDate()))
-      })
-    })
+        const taskName = `${task.getName()} (${project.getName()})`;
+        this.getProject('This week').addTask(new Task(taskName, task.getDate()));
+      });
+    });
 
     this.getProject('This week').setTasks(
       this.getProject('This week')
         .getTasks()
-        .sort((taskA, taskB) =>
-          compareAsc(
-            toDate(new Date(taskA.getDateFormatted())),
-            toDate(new Date(taskB.getDateFormatted()))
-          )
-        )
-    )
+        .sort((taskA, taskB) => compareAsc(
+          toDate(new Date(taskA.getDateFormatted())),
+          toDate(new Date(taskB.getDateFormatted())),
+        )),
+    );
   }
 }
