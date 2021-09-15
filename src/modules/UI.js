bcf1fdf9ec0871b068d99f4ebac88b51f129b18d
tmampa <tmampa94@gmail.com>
2021-09-15 12:10:08 +0200

    Load content

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,3 +2,75 @@ import { format } from 'date-fns'
 import Storage from './Storage'
 import Project from './Project'
 import Task from './Task'
+
+export default class UI {
+  // LOADING CONTENT
+
+  static loadHomepage() {
+    UI.loadProjects()
+    UI.initProjectButtons()
+    UI.openProject('Inbox', document.getElementById('button-inbox-projects'))
+    document.addEventListener('keydown', UI.handleKeyboardInput)
+  }
+
+  static loadProjects() {
+    Storage.getTodoList()
+      .getProjects()
+      .forEach((project) => {
+        if (
+          project.name !== 'Inbox' &&
+          project.name !== 'Today' &&
+          project.name !== 'This week'
+        ) {
+          UI.createProject(project.name)
+        }
+      })
+
+    UI.initAddProjectButtons()
+  }
+
+  static loadTasks(projectName) {
+    Storage.getTodoList()
+      .getProject(projectName)
+      .getTasks()
+      .forEach((task) => UI.createTask(task.name, task.dueDate))
+
+    if (projectName !== 'Today' && projectName !== 'This week') {
+      UI.initAddTaskButtons()
+    }
+  }
+
+  static loadProjectContent(projectName) {
+    const projectPreview = document.getElementById('project-preview')
+    projectPreview.innerHTML = `
+          <h1 id="project-name">${projectName}</h1>
+          <div class="tasks-list" id="tasks-list"></div>`
+
+    if (projectName !== 'Today' && projectName !== 'This week') {
+      projectPreview.innerHTML += `
+          <button class="button-add-task" id="button-add-task">
+            <i class="fas fa-plus"></i>
+            Add Task
+          </button>
+          <div class="add-task-popup" id="add-task-popup">
+            <input
+              class="input-add-task-popup"
+              id="input-add-task-popup"
+              type="text"
+            />
+            <div class="add-task-popup-buttons">
+              <button class="button-add-task-popup" id="button-add-task-popup">
+                Add
+              </button>
+              <button
+                class="button-cancel-task-popup"
+                id="button-cancel-task-popup"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>`
+    }
+
+    UI.loadTasks(projectName)
+  }
